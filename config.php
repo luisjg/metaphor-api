db3f4e952caea4c32b25beaad5ad064ff99b3132
luisjg <luis.guzman@metalab.csun.edu>
2017-04-21 10:09:39 -0700

    Include the config file.

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'production' => false,
+    'google_analytics' => 'UA-77781058-1',
+    'setActiveClass' => function ($page, $section, $active = 'nav__item--active') {
+      return str_contains($page->getPath(), 'docs/'.$section) ? $active : '';
+    }
+];
